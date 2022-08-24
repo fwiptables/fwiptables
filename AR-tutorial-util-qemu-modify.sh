ec2d1f1afa1b318745162d700d01cbc07b949c90
root <fwiptables@gmx.com>
2022-08-24 12:09:09 +0200

    nuevos archivos: AR-tutorial-util-qemu-modify.sh

---
+++ AR-tutorial-util-qemu-modify.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+source /etc/profile
+
+#### para valor vacio
+if [ "$1" == "$NULL" ]; then echo  "### launch: $0 status|start|stop|restart|mount|umount  imagen-hd [ imagen.iso ]"; exit ; fi
+
+##### variables a a modififcar ####  custom here and modify it ####
+qemu_name="Qemu VM linux debian"
+architecture="qemu-system-i386"
+qemu_vm="/qemu/debian-i386.qcow2"
+qemu_cdrom="$NULL"
+###################################################################
+
+#### variables estaticas
+if [ "$qemu_cdrom"  == "$NULL" ]; then qemu_cdrom="" ; else qemu_cdrom="-cdrom $qemu_cdrom -boot d"; fi
+command_qemu="$(command -v $architecture)"
+qemu_options="-m 2G -machine type=pc,accel=kvm -k es -smp 4 -rtc base=localtime -display vnc=0.0.0.0:0"
+qemu_full="$command_qemu $qemu_options $qemu_vm $qemu_cdrom"
+
+##### #### status|stop|start|restart
+
+##### with status
+if [ "$1" == "status" ]; then
+ps -af | grep -i  $architecture
+exit; fi
+
+##### with stop
+if [ "$1" = "stop" ]; then killall -9 $architecture &> /dev/null
+echo "[ ok ] [ stopped ] [ La maquina virtual detenida: $qemu_name ]"
+exit; fi
+
+##### with start
+if [ "$1" = "start" ]; then $qemu_full &
+echo "[ ok ] [ started ] [ La maquina virtual iniciada: $qemu_name ]"
+exit; fi
+
+##### with restart
+if [ "$1" = "restart" ] ; then
+$0 stop &> /dev/null
+$0 start & > /dev/null
+echo "[ ok ] [ restarted ] [ La maquina virtual reiniciada: $qemu_name ]"
+exit; fi
+
+#### #### mount:
+#### secuencia de montaje de nbd
+if [ "$1" = "mount" ] && [  -f "$2" ]; then
+
+#### carga el modulo del kernel
+modprobe nbd max_part=8
+
+#### conecta un disco
+qemu-nbd --connect=/dev/nbd0 $2
+
+#### lista las particiones
+fdisk /dev/nbd0 -l
+
+#### monta la imagen de qemu
+mkdir -p $HOME/mnt/nbd0p1 &> /dev/null
+MKDIR -P $HOME/mnt/nbd0p2 &> /dev/null
+umount $HOME/mnt/nbd0p* &> /dev/null
+mount /dev/nbd0p1 $HOME/mnt/nbd0p1/ &> /dev/null
+mount /dev/nbd0p2 $HOME/mnt/nbd0p2/ &> /dev/null
+echo "OK. qemu image mounted in $HOME/mnt/nbd0p1 and nbd0p2"
+exit; fi
+
+#### #### umount:
+#### desmonta y desconecta la imagen de qemu
+if [ "$1" = "umount" ]; then
+umount $HOME/mnt/nbd0p* &> /dev/null
+qemu-nbd --disconnect /dev/nbd0 &> /dev/null
+echo "Ok. Umounted $HOME/mnt/nbd0p*"
+exit; fi
+
+#### otros parametros
+$0
+
+##### end script
+exit
