59dc3c3fd84545dd17d3c3ccd75f3a4c597ca7dd
root <fwiptables@gmx.com>
2023-11-06 07:51:38 +0100

    compile

--- fwiptables-ver-10_10-bash.sh
+++ fwiptables-ver-10_10-bash.sh
@@ -5240,17 +5240,19 @@ exit; fi
 ####
 if   [ "$first_option" == "compile" ]; then 
 echo "$title_md [ $first_option ] [ howto compile bash script with obash ] "
-echo "$text_md This script bash run with ./fwiptables-cmd it works, but if you desire compile..."
+echo "$title_md This script bash run with ./fwiptables-cmd it works, but if you desire compile..."
 echo "$text_md"
-echo "$text_md Three steps to compile fwiptables script, it is possible with obash."
+echo "$title_md Three steps to compile fwiptables script, it is possible with obash."
 echo "$text_md"
-echo "$text_md necesary:     fwiptables in source script bash."
-echo "$text_md 1. first:     download obash from internet."
-echo "$text_md 2.second:     compile obash command with make."
-echo "$text_md 3. third:     launch:# obash -c -o ./destination-file.bin fwiptables-ver-num.sh"
+echo "$text_md  necesary:     fwiptables in source script bash."
+echo "$text_md 1.  first:     download obash from internet."
+echo "$text_md 2. second:     compile obash command with make."
+echo "$text_md 3.  third:     launch:# obash -c -o ./destination-file.bin fwiptables-ver-num.sh"
 echo "$text_md"
-echo "$text_md Actually, the file type is:"
+echo "$title_md Actually, the file type is:"
 file -L $cmd_binary
+echo "$text_md #"
+echo "$text_md #"
 exit; fi
 ####
 ####
