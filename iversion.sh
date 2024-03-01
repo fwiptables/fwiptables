941d5bb5123cd9b26700d5e1ac4fcf5fa82e1e22
root <fwiptables@gmx.com>
2024-03-01 10:59:49 +0100

    update

--- iversion.sh
+++ iversion.sh
@@ -2536,9 +2536,9 @@ echo "$text_md info date intro filelog autolog ip4 ip6 speed-ip4 speed-ip6      
 echo "$text_md free sockets nodes geoip ip-forward version notes code               "
 echo "$text_md treeconf depends commands variables license uninstall install        "
 echo "$title_md  [ options-expert ] expert                                          "
-echo "$title_md  ||| Example: fwiptables gui-zenity info      |||                   "
-echo "$title_md  ||| GPL v2, License General Public version 2 |||                   "
-echo "$title_md  ||| Report bugs at $cmd_contact              |||"
+echo "$title_md  ||| Example: $cmd_basename gui-zenity info    "
+echo "$title_md  ||| Reports: Report bugs at $cmd_contact   "
+echo "$title_md  ||| License: $cmd_license                  "
 exit ; fi
 ####
 ####
