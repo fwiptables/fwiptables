287f49a573793c70915fa38e45316758b816aaf8
root <fwiptables@gmx.com>
2024-03-13 18:34:58 +0100

    update

--- iversion.sh
+++ iversion.sh
@@ -768,16 +768,14 @@ fi
 ####
 ####
 case "$first_option" in
-"ls4") list_rules_conceptual=""; first_option="list4" ;;
-"ls6") list_rules_conceptual=""; first_option="list6" ;;
 "lsn4") list_rules_conceptual="no"; first_option="listn4" ;;
 "lsn6") list_rules_conceptual="no"; first_option="listn6" ;;
 list-*) list_rules_conceptual="" ;;
 listn-*)  list_rules_conceptual="no" ;;
 esac
 case "$second_option" in
-"ls4") list_rules_conceptual=""; second_option="list4" ;;
-"ls6") list_rules_conceptual=""; second_option="list6" ;;
+"ls4") list_rules_conceptual=""; second_option="ls4" ;;
+"ls6") list_rules_conceptual=""; second_option="ls6" ;;
 "lsn4") list_rules_conceptual="no"; second_option="listn4" ;;
 "lsn6") list_rules_conceptual="no"; second_option="listn6" ;;
 list-*) list_rules_conceptual="" ;;
@@ -968,6 +966,14 @@ esac
 ##########    ALIAS CONFIG
 ##########    ALIAS CONFIG
 #### :rutina-inicial-alias-config:
+####
+####
+case $first_option in 
+"ls4") $cmd_realpath list4 &> $temporal_text-list4
+cat $temporal_text-list4 | $command_grep -E -v Warning: ; exit ;;
+"ls6") $cmd_realpath list6 &> $temporal_text-list6
+cat $temporal_text-list6 | $command_grep -E -v Warning: ; exit ;;
+esac ; 
 ####
 ####
 #### english:  alias simple for regenerate configurations templates o esquive
