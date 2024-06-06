c120121c4b3961e179c19f2eff1acad6f9e2709a
root <fwiptables@gmx.com>
2024-06-06 10:13:01 +0200

    update

--- iversion.sh
+++ iversion.sh
@@ -101,9 +101,9 @@ echo ; fi
 cmd_realpath="$(realpath $0)"                # full routename
 cmd_basename="$(basename $0)"                # only filename
 #### number version
-cmd_dev=""                                   # -dev version or null final version
+cmd_dev="-dev"                                   # -dev version or null final version
 cmd_mayor="13"                               # number mayor version
-cmd_minor="02"                               # number minor version
+cmd_minor="03"                               # number minor version
 cmd_year="2024"                              # number year version
 cmd_month="06"                               # number mouth version
 cmd_version="$cmd_mayor-$cmd_minor$cmd_dev"  # final number version
