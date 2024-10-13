a75631f3879d778c4c37349f60799ac7498b6cf7
root <fwiptables@gmx.com>
2024-10-13 13:36:38 +0200

    updatge

--- iversion.sh
+++ iversion.sh
@@ -95,7 +95,7 @@ fi
 #### The number version
 cmd_year="24"                                              # Number year version
 cmd_month="10"                                             # Number mouth version
-cmd_letter="F"                                             # Number letter version
+cmd_letter="G"                                             # Number letter version
 cmd_version="$cmd_year-$cmd_month-$cmd_letter"             # Final date like number version
 cmd_date="Year 20$cmd_year / Month $cmd_month"
 #### The data version
