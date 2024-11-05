7ba1b380acd1ebed6703ba307213a031d096cc64
root <fwiptables@gmx.com>
2024-11-05 11:19:25 +0100

    update

--- iversion.sh
+++ iversion.sh
@@ -6647,15 +6647,24 @@ if [ "$first_option" == "add-whitelist4" ] || [ "$first_option" == "expert-add-w
 ####
 ####
 if [ "$2" == "$NULL" ]; then echo "$title_md $text_fail type host ip4 or net ip4 to be in whitelist"; exit ; fi
-echo "$title_md [ Working ] ADD ipv4 rules whitelist: ACCEPT to $2"
-$command_ip4tablesnft    -t filter -I INPUT 2 -s $2   -m comment --comment "add-whitelist4"  \
--j ACCEPT &> /dev/null && echo ok rule nft 1/4 with $2 || echo without rule nft 1/4
-$command_ip4tablesnft    -t filter -I OUTPUT 2 -d $2  -m comment --comment "add-whitelist4"  \
--j ACCEPT &> /dev/null && echo ok rule nft 2/4 with $2 || echo without rule nft 2/4
-$command_ip4tableslegacy -t filter -I INPUT 2 -s $2   -m comment --comment "add-whitelist4"  \
--j ACCEPT &> /dev/null && echo ok rule legacy 3/4 with $2 || echo without rule legacy 3/4
-$command_ip4tableslegacy -t filter -I OUTPUT 2 -d $2  -m comment --comment "add-whitelist4"  \
--j ACCEPT &> /dev/null && echo ok rule legacy 4/4 with $2 || echo without rule legacy 4/4
+####
+####
+host_ip="$(echo $2 | sed 's/,/ /g')"
+for add in $host_ip ; do
+####
+####
+echo "$title_md [ Working ] ADD ipv4 rules whitelist: ACCEPT to $add"
+$command_ip4tablesnft    -t filter -I INPUT 2 -s $add   -m comment --comment "add-whitelist4"  \
+-j ACCEPT &> /dev/null && echo ok rule nft 1/4 with $add || echo without rule nft 1/4
+$command_ip4tablesnft    -t filter -I OUTPUT 2 -d $add  -m comment --comment "add-whitelist4"  \
+-j ACCEPT &> /dev/null && echo ok rule nft 2/4 with $add || echo without rule nft 2/4
+$command_ip4tableslegacy -t filter -I INPUT 2 -s $add   -m comment --comment "add-whitelist4"  \
+-j ACCEPT &> /dev/null && echo ok rule legacy 3/4 with $add || echo without rule legacy 3/4
+$command_ip4tableslegacy -t filter -I OUTPUT 2 -d $add  -m comment --comment "add-whitelist4"  \
+-j ACCEPT &> /dev/null && echo ok rule legacy 4/4 with $add || echo without rule legacy 4/4
+####
+####
+done
 ####
 ####
 exit; fi
@@ -6671,27 +6680,30 @@ if [ "$first_option" == "add-whitelist6" ] || [ "$first_option" == "expert-add-w
 ####
 ####
 if [ "$2" == "$NULL" ]; then echo "$title_md $text_fail type host ip6 or net ip6 to be in whitelist"; exit ; fi
-echo "$title_md [ Working ] ADD ipv6 rules whitelist: ACCEPT to $2"
-$command_ip6tablesnft    -t filter -I INPUT 2 -s $2   -m comment --comment "add-whitelist6"  \
--j ACCEPT  &> /dev/null && echo ok rule nft 1/4 with $2 || echo without rule nft 1/4
-$command_ip6tablesnft    -t filter -I OUTPUT 2 -d $2  -m comment --comment "add-whitelist6"  \
--j ACCEPT  &> /dev/null && echo ok rule nft 2/4 with $2 || echo without rule nft 2/4
-$command_ip6tableslegacy -t filter -I INPUT 2 -s $2   -m comment --comment "add-whitelist6"  \
--j ACCEPT  &> /dev/null && echo ok rule legacy 3/4 with $2 || echo without rule nft 3/4
+####
+####
+host_ip="$(echo $2 | sed 's/,/ /g')"
+for add in $host_ip;  do
+####
+####
+echo "$title_md [ Working ] ADD ipv6 rules whitelist: ACCEPT to $add"
+$command_ip6tablesnft    -t filter -I INPUT 2 -s $add   -m comment --comment "add-whitelist6"  \
+-j ACCEPT  &> /dev/null && echo ok rule nft 1/4 with $add    || echo without rule nft 1/4
+$command_ip6tablesnft    -t filter -I OUTPUT 2 -d $add  -m comment --comment "add-whitelist6"  \
+-j ACCEPT  &> /dev/null && echo ok rule nft 2/4 with $add    || echo without rule nft 2/4
+$command_ip6tableslegacy -t filter -I INPUT 2 -s $add   -m comment --comment "add-whitelist6"  \
+-j ACCEPT  &> /dev/null && echo ok rule legacy 3/4 with $add || echo without rule nft 3/4
 $command_ip6tableslegacy -t filter -I OUTPUT 2 -d $2  -m comment --comment "add-whitelist6"  \
--j ACCEPT &> /dev/null && echo ok rule legacy 4/4 with $2 || echo without rule nft 4/4
+-j ACCEPT &> /dev/null && echo ok rule legacy 4/4 with $add  || echo without rule nft 4/4
+####
+####
+done
 ####
 ####
 exit; fi
 ####
 ####
 #### :rutina-final-add-wihtelist6
-
-
-
-
-
-
 ##########    english: add-blacklist4: blacklist ipv4      ##########
 ##########    spanish: add-blacklist4: lista negra ipv4    ##########
 #### :rutina-inicial-add-blacklist4
@@ -6704,15 +6716,22 @@ if [ "$2" == "$NULL" ]; then
 echo "$title_md $text_fail type host ip4 or net ip4 to be in blacklist" ; exit ; fi
 ####
 ####
-echo "$title_md [ Working ] ADD ipv4 rules blacklist: DROP to $2"
-$command_ip4tablesnft    -t filter -I INPUT 2 -s $2  -m comment --comment "add-blacklist4" \
--j DROP  &> /dev/null && echo ok rule nft 1/4 with $2 || echo without rule nft 1/4
-$command_ip4tablesnft    -t filter -I OUTPUT 2 -d $2 -m comment --comment "add-blacklist4" \
--j DROP  &> /dev/null && echo ok rule nft 2/4 with $2 || echo without rule nft 2/4
-$command_ip4tableslegacy -t filter -I INPUT 2 -s $2  -m comment --comment "add-blacklist4"  \
--j DROP  &> /dev/null && echo ok rule legacy 3/4 with $2 || echo without rule legacy 3/4
-$command_ip4tableslegacy -t filter -I OUTPUT 2 -d $2 -m comment --comment "add-blacklist4" \
--j DROP &> /dev/null  && echo ok rule legacy 4/4 with $2 || echo without rule legacy 4/4
+host_ip="$(echo $2 | sed 's/,/ /g')"
+for add in $host_ip ; do
+####
+####
+echo "$title_md [ Working ] ADD ipv4 rules blacklist: DROP to $add"
+$command_ip4tablesnft    -t filter -I INPUT 2 -s $add  -m comment --comment "add-blacklist4" \
+-j DROP  &> /dev/null && echo ok rule nft 1/4 with $add    || echo without rule nft 1/4
+$command_ip4tablesnft    -t filter -I OUTPUT 2 -d $add -m comment --comment "add-blacklist4" \
+-j DROP  &> /dev/null && echo ok rule nft 2/4 with $add    || echo without rule nft 2/4
+$command_ip4tableslegacy -t filter -I INPUT 2 -s $add  -m comment --comment "add-blacklist4"  \
+-j DROP  &> /dev/null && echo ok rule legacy 3/4 with $add || echo without rule legacy 3/4
+$command_ip4tableslegacy -t filter -I OUTPUT 2 -d $add -m comment --comment "add-blacklist4" \
+-j DROP &> /dev/null  && echo ok rule legacy 4/4 with $add || echo without rule legacy 4/4
+####
+####
+done
 ####
 ####
 exit; fi
@@ -6731,15 +6750,22 @@ if [ "$2" == "$NULL" ]; then
 echo "$title_md $text_fail type host ip6 or net ip6 to be in blacklist"; exit ; fi
 ####
 ####
-echo "$title_md [ Working ] ADD ipv6 rules blacklist: DROP to $2"
-$command_ip6tablesnft    -t filter -I INPUT 2 -s $2  -m comment --comment "add-blacklist6" \
--j DROP  &> /dev/null && echo ok rule nft 1/4 with $2 || echo without rule nft 1/4
-$command_ip6tablesnft    -t filter -I OUTPUT 2 -d $2 -m comment --comment "add-blacklist6" \
--j DROP  &> /dev/null && echo ok rule nft 2/4 with $2 || echo without rule nft 2/4
-$command_ip6tableslegacy -t filter -I INPUT 2 -s $2  -m comment --comment "add-blacklist6" \
--j DROP &> /dev/null && echo ok rule legacy 3/4 with $2 || echo without rule legacy 3/4
-$command_ip6tableslegacy -t filter -I OUTPUT 2 -d $2 -m comment --comment "add-blacklist6" \
--j DROP &> /dev/null && echo ok rule legacy 4/4 with $2 || echo without rule legacy 4/4
+host_ip="$(echo $2 | sed 's/,/ /g')"
+for add in $host_ip ; do
+####
+####
+echo "$title_md [ Working ] ADD ipv6 rules blacklist: DROP to $add"
+$command_ip6tablesnft    -t filter -I INPUT 2 -s $add  -m comment --comment "add-blacklist6" \
+-j DROP  &> /dev/null && echo ok rule nft 1/4 with $add   || echo without rule nft 1/4
+$command_ip6tablesnft    -t filter -I OUTPUT 2 -d $add -m comment --comment "add-blacklist6" \
+-j DROP  &> /dev/null && echo ok rule nft 2/4 with $add   || echo without rule nft 2/4
+$command_ip6tableslegacy -t filter -I INPUT 2 -s $add  -m comment --comment "add-blacklist6" \
+-j DROP &> /dev/null && echo ok rule legacy 3/4 with $add || echo without rule legacy 3/4
+$command_ip6tableslegacy -t filter -I OUTPUT 2 -d $add -m comment --comment "add-blacklist6" \
+-j DROP &> /dev/null && echo ok rule legacy 4/4 with $add || echo without rule legacy 4/4
+####
+####
+done
 ####
 ####
 exit; fi
